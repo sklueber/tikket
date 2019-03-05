1f1ecef9b1ef4871c573aa1efb46dbea0d6eae5e
Max Stockhausen <info@max-stockhausen.de>
2019-03-05 22:18:20 +0100

    Datenbankverbindung eingefügt

--- src/tikketClient.java
+++ src/tikketClient.java
@@ -1,4 +1,36 @@
+import java.sql.*;
+
 public class tikketClient {
-    public static void main(String[] args) {
+        /**
+         * Feinste Kopierarbeit
+         * Connect to a sample database
+         */
+        public static void connect() {
+            Connection conn = null;
+            try {
+                // db parameters
+                String url = "jdbc:sqlite:tikket_db.db";
+                // create a connection to the database
+                conn = DriverManager.getConnection(url);
+
+                System.out.println("Connection to SQLite has been established.");
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+        /**
+         * @param args the command line arguments
+         */
+        public static void main(String[] args) {
+            connect();
+        }
     }
-}
