ef7f1584d16f02519fd0725319eb59ccf825c860
max-stockhausen <info@max-stockhausen.de>
2019-03-06 10:14:27 +0100

    Server erstellt

--- src/tikketClient.java
+++ src/tikketClient.java
@@ -1,29 +1,3 @@
-import java.sql.*;
-
 public class tikketClient {
 
-        public static void connect() {
-            Connection conn = null;
-            try {
-                String url = "jdbc:sqlite:tikket_db.db"; //Location der Datenbank
-                conn = DriverManager.getConnection(url);
-
-                System.out.println("Verbindung zur tikket-Datenbank hergestellt"); //Success
-
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            } finally {
-                try {
-                    if (conn != null) {
-                        conn.close();
-                    }
-                } catch (SQLException ex) {
-                    System.out.println(ex.getMessage()); //Failure
-                }
-            }
-        }
-
-        public static void main(String[] args) {
-            connect();
-        }
     }
