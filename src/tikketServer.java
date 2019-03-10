5531bb4de3b8297666ba091344fa64aab1c9ed22
Max Stockhausen <info@max-stockhausen.de>
2019-03-10 23:09:35 +0100

    Methoden zum einfügen und auslesen von Daten hinzugefügt

--- src/tikketServer.java
+++ src/tikketServer.java
@@ -1,39 +1,80 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class tikketServer {
-    public static void connect() {
+
+    public static void main(String[] args) {
+    connect();
+//    veranstalterErstellen("MaxStockhausenIst1nicerDude");
+//    veranstalterAusgeben();
+
+}
+
+    public static Connection connect() {
         Connection conn = null;
         try {
             String url = "jdbc:sqlite:tikket_db.db"; //Location der Datenbank
             conn = DriverManager.getConnection(url);
-
-            System.out.println("Verbindung zur tikket-Datenbank hergestellt"); //Success
-
+            System.out.println("Verbindung zur tikket-Datenbank hergestellt");
+            return conn;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage()); //Failure
+        }
+    return conn;
+    }
+
+
+    private void ticketErstellen() {
+    }
+
+    private void ticketPruefen() {
+    }
+
+    private void ticketAusgeben() {
+    }
+
+    private void veranstaltungErstellen() {
+    }
+
+    private void veranstaltungAusgeben() {
+
+    }
+
+    private static void veranstalterErstellen(String vr_name) {
+        String sql = "INSERT INTO veranstalter(vr_name) VALUES(?)";
+
+        try (Connection conn = connect()) {
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, vr_name);
+                pstmt.executeUpdate();
             }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    public static void main(String[] args) {
-        connect();
+    private static void veranstalterAusgeben() {
+        String sql = "SELECT vr_ID, vr_name FROM  veranstalter";
+
+        try (Connection conn = connect()) {
+            try (Statement stmt = conn.createStatement()) {
+                try (ResultSet rs = stmt.executeQuery(sql)) {
+                    //ResultSet durchloopen
+                    while (rs.next()) {
+                        System.out.println(rs.getInt("vr_ID") + "\t" + rs.getString("vr_name") + "\t");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private void ticketErstellen(){};
+    private void veranstalterEntfernen(){
 
-    private void ticketPruefen() {};
-
-    private void ticketAusgeben() {};
-
-    private void veranstaltungErstellen() {};
-
-    private void veranstaltungAusgeben() {};
+    }
+}
