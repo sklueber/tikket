6df62aba3f258ba2accaf5f436c0e085c90875de
Simon <simon.klueber@t-online.de>
2019-03-20 22:18:24 +0100

    zwei von StackOverflow kopierte Varianten des Mailversands, deren Compilerfehler dank dem Hinzufügen von 2 neuen Libraries glücklicherweise gelöst wurden Allerdings laufen die Programme einfach nur vor sich hin und machen nichts, werfen also nicht einmal einen Fehler. Serverdaten etc wurden in den SSL-Versionen benutzt, was aber sowieso egal zu sein scheint, weil es ja keine Fehler gibt...

---
+++ src/Mail2.java
@@ -0,0 +1,92 @@
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class Mail2 {
+    public static void main(String[] args) {
+        // Recipient's email ID needs to be mentioned.
+        String to = "s3.hans@web.de";
+
+        // Sender's email ID needs to be mentioned
+        String from = "tikket@stockhausenmail.de";
+
+        final String username = "tikket@stockhausenmail.de";//change accordingly
+        final String password = "TikketSystem1!";//change accordingly
+
+        // Assuming you are sending email through relay.jangosmtp.net
+        String host = "smtprelaypool.ispgateway.de";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "465");
+
+        // Get the Session object.
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+
+            // Set Subject: header field
+            message.setSubject("Testing Subject");
+
+            // Create the message part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Now set the actual message
+            messageBodyPart.setText("This is message body");
+
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "/home/manisha/file.txt";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            message.setContent(multipart);
+
+            // Send message
+            Transport.send(message);
+
+            System.out.println("Sent message successfully....");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
