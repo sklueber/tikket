1aabdaec48f40c9e4bf72eba3efaf1ba672b3313
R10604 <simon.klueber@t-online.de>
2019-03-20 11:17:27 +0100

    Barcodeerstellung

---
+++ src/CodeGenerator.java
@@ -0,0 +1,33 @@
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.Code128Writer;
+import com.google.zxing.qrcode.*;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class CodeGenerator {
+    private static final String QR_CODE_IMAGE_PATH = "./MyQRCode.png";
+
+    private static void generateBarcodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        Code128Writer BarcodeWriter = new Code128Writer();
+        BitMatrix bitMatrix = BarcodeWriter.encode(text, BarcodeFormat.CODE_128, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    public static void main(String[] args) {
+        try {
+            generateBarcodeImage("This is my first Barcode leeeel", 350, 350, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate Code, IOException :: " + e.getMessage());
+        }
+    }
+}
