05d9ba160bee76527ad05ead29516d4028cb180b
Simon <simon.klueber@t-online.de>
2019-03-21 20:42:13 +0100

    Mailversand über SSL inklusive Anhang funktioniert!

--- src/Mailversand.java
+++ src/Mailversand.java
@@ -1,61 +1,94 @@
-import java.util.*;
-import javax.mail.*;
-import javax.mail.internet.*;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 public class Mailversand {
-
-    private static String USER_NAME = "tikket.stockhausenmail.de";  // GMail user name (just the part before "@gmail.com")
-    private static String PASSWORD = "TikketSystem1!"; // GMail password
-    private static String RECIPIENT = "s3.hans@web.de";
-
     public static void main(String[] args) {
-        String from = USER_NAME;
-        String pass = PASSWORD;
-        String[] to = { RECIPIENT }; // list of recipient email addresses
-        String subject = "Java send mail example";
-        String body = "Welcome to JavaMail!";
+        // Recipient's email ID needs to be mentioned.
+        String to = "nico.plum@gao-online.de";
 
-        sendFromGMail(from, pass, to, subject, body);
-    }
+        // Sender's email ID needs to be mentioned
+        String from = "noreply.tikket@stockhausenmail.de";
 
-    private static void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
-        Properties props = System.getProperties();
-        String host = "smtp.sslout.df.eu";
+        final String username = "tikket@stockhausenmail.de";//change accordingly
+        final String password = "TikketSystem1!";//change accordingly
+
+        // Assuming you are sending email through relay.jangosmtp.net
+        String host = "sslout.df.eu";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", host);
-        props.put("mail.smtp.user", from);
-        props.put("mail.smtp.password", pass);
         props.put("mail.smtp.port", "465");
-        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
 
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage message = new MimeMessage(session);
+        // Get the Session object.
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
 
         try {
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
-            InternetAddress[] toAddress = new InternetAddress[to.length];
 
-            // To get the array of addresses
-            for( int i = 0; i < to.length; i++ ) {
-                toAddress[i] = new InternetAddress(to[i]);
-            }
+            // Set To: header field of the header.
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
 
-            for( int i = 0; i < toAddress.length; i++) {
-                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
-            }
+            // Set Subject: header field
+            message.setSubject("[TIKKET.] Vielen Dank für ihre Bestellung! Jetzt auch mit SSL!");
 
-            message.setSubject(subject);
-            message.setText(body);
-            Transport transport = session.getTransport("smtp");
-            transport.connect(host, from, pass);
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-        }
-        catch (AddressException ae) {
-            ae.printStackTrace();
-        }
-        catch (MessagingException me) {
-            me.printStackTrace();
+            // Create the message part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Now set the actual message
+            messageBodyPart.setText("ur mom gay");
+
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            // Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "./MyBarcode.png";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+
+            // Send the complete message parts
+            message.setContent(multipart);
+
+            // Send message
+            Transport.send(message);
+
+            System.out.println("Sent message successfully....");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/TestklasseDieDurchEinfachGUIAufraufeErsetztWerdenMuss.java
+++ src/TestklasseDieDurchEinfachGUIAufraufeErsetztWerdenMuss.java
@@ -1,8 +1,8 @@
 public class TestklasseDieDurchEinfachGUIAufraufeErsetztWerdenMuss
 {
-public static void main(String args[]){
-    CodeGenerator.barcodeErstellen("ey");
-    Drucker d = new Drucker();
-    d.drucken("MyBarcode.png");
+public static void main(String[] args){
+    CodeGenerator.barcodeErstellen("warum scannst du das? haste nichts besseres zu tun?");
+    //Drucker d = new Drucker();
+    //d.drucken("MyBarcode.png");
 }
 }
