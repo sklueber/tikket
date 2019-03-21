ad56897041258858cee966251abe8fbc0894f628
Simon <simon.klueber@t-online.de>
2019-03-21 22:47:30 +0100

    Der Mailversand erhält die Empfängermail und die Veranstaltung um die es ging nun als Parameter. In der GUI muss das wie in der passend bezeichneten Klasse geschehen.                                                                                                                                                                   In der GUI muss vorher geprüft werden, ob es sich um eine gültige E-Mail-Adresse handelt. Der Dateipfad des Anhangs müsste möglicherweise noch als 3. Parameter übergeben werden, falls wir mehrer Barcodes unter verschiedenen Bezeichnungen speichern werden.                                                                                                                                                                   Der Name des Anhangs in der Mail ist schöner und MyBarcode.png wird mitcommitet damit man beim Testen auf anderen Maschinen nicht erst einen Code generieren muss.

--- src/Mailversand.java
+++ src/Mailversand.java
@@ -1,19 +1,12 @@
-import java.util.Properties;
-
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
-import javax.mail.BodyPart;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
 
 public class Mailversand {
     public static void main(String[] args) { //args: 0: Empfänger, 1: Veranstaltungsname
@@ -27,7 +20,7 @@ public class Mailversand {
         final String password = "TikketSystem1!";//change accordingly
 
 
-        String host = "sslout.df.eu";
+        String host = "sslout.df.eu"; //TODO persönliche Daten in txt
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
