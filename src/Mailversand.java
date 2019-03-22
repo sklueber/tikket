d9813afd51a24c15f949598a814aaf4477329b55
Simon <simon.klueber@t-online.de>
2019-03-22 17:56:52 +0100

    alle persönlichen Daten sind in eigener txt-Datei

--- src/Mailversand.java
+++ src/Mailversand.java
@@ -6,27 +6,44 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Mailversand {
     public static void main(String[] args) { //args: 0: Empfänger, 1: Veranstaltungsname
+        //Auslesen von MailserverSettings.txt
+        String txtfrom, txtusername, txtpassword, txthost, txtport;
+        String[] arr = new String[0];
+        for (int i = 0; i < 4; i++) {
+            arr = Mailversand.txtLesen();
+            //System.out.println(arr[i]);
+        }
+        txtfrom = arr[0].replaceAll("\\bfrom: \\b", "");
+        txtusername = arr[1].replaceAll("\\busername: \\b", "");
+        txtpassword = arr[2].replaceAll("\\bpassword: \\b", "");
+        txthost = arr[3].replaceAll("\\bhost: \\b", "");
+        txtport = arr[4].replaceAll("\\bport: \\b", "");
+
+
         // Recipient's email ID needs to be mentioned.
         String to = args[0];
 
         // Sender's email ID needs to be mentioned
-        String from = "noreply.tikket@stockhausenmail.de";
+        String from = txtfrom;
 
-        final String username = "tikket@stockhausenmail.de";//change accordingly
-        final String password = "TikketSystem1!";//change accordingly
+        final String username = txtusername;//change accordingly
+        final String password = txtpassword;//change accordingly
 
 
-        String host = "sslout.df.eu"; //TODO persönliche Daten in txt
+        String host = txthost;
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.port", txtport);
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.socketFactory.fallback", "false");
 
@@ -83,5 +100,20 @@ public class Mailversand {
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String[] txtLesen(){
+        String[] serverdaten = new String[5];
+        int i = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader("./MailserverSettings.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                serverdaten[i] = line;
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return serverdaten;
     }
 }
