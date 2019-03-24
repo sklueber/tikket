5a70301e3bcab6743e30f24ca3136c59568e0fbe
Simon <simon.klueber@t-online.de>
2019-03-24 17:07:23 +0100

    Abgerundete Ecken! und ansonsten Vorbereitung auf Max' Methoden. Im Moment wird jedes Ticket mit der Nummer 100 akzeptiert, Abbruch wird auch dargestellt

--- app/src/main/java/com/tikket/tikketscan/MainActivity.java
+++ app/src/main/java/com/tikket/tikketscan/MainActivity.java
@@ -1,10 +1,13 @@
 package com.tikket.tikketscan;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,19 +16,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView status = findViewById(R.id.textViewStatus);
+        status.setBackgroundResource(R.drawable.rounded_corner);
+        GradientDrawable drawable = (GradientDrawable) status.getBackground();
+        drawable.setColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
     }
     public void onClick (View vie){
         IntentIntegrator scanDing = new IntentIntegrator(this);
         scanDing.initiateScan();
     }
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (scanResult != null) {
-            String barcode;
-            barcode = scanResult.getContents();
-            TextView textViewTktNr = findViewById(R.id.textViewTktNr);
-            textViewTktNr.setText(barcode);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { //Ergebnis des Barcode-Aufrufs wird verarbeitet
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            TextView tktNr = findViewById(R.id.textViewTktNr);
+            if(result.getContents() == null) {
+                tktNr.setText(R.string.cancelled);
+                statusAnzeigen("ERROR");
+            } else {
+                tktNr.setText(result.getContents());
+                statusAnzeigen(result.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    /*public void statusAnzeigen(String pTicketnummer){
+        TextView status = findViewById(R.id.textViewStatus);
+    if(!android.text.TextUtils.isDigitsOnly(pTicketnummer)){ //ob das gescannte Ergebnis überhaupt aus Zahlen besteht
+        status.setBackgroundColor(ContextCompat.getColor(this, R.color.colorCancelYellow));
+        status.setText(R.string.cancelled);
+    } else {
+        if(pTicketnummer.equals("100")){
+            status.setBackgroundColor(ContextCompat.getColor(this, R.color.colorValidGreen));
+            status.setText(R.string.valid);
+        } else {
+            status.setBackgroundColor(ContextCompat.getColor(this, R.color.colorInvalidRed));
+            status.setText(R.string.invalid);
+        }
+    }
+    }*/
+    public void statusAnzeigen(String pTicketnummer){
+        TextView status = findViewById(R.id.textViewStatus);
+        GradientDrawable drawable = (GradientDrawable) status.getBackground();
+        if(!android.text.TextUtils.isDigitsOnly(pTicketnummer)){ //ob das gescannte Ergebnis überhaupt aus Zahlen besteht
+            drawable.setColor(ContextCompat.getColor(this, R.color.colorCancelYellow));
+            status.setText(R.string.cancelled);
+        } else {
+            if(pTicketnummer.equals("100")){
+                drawable.setColor(ContextCompat.getColor(this, R.color.colorValidGreen));
+                status.setText(R.string.valid);
+            } else {
+                drawable.setColor(ContextCompat.getColor(this, R.color.colorInvalidRed));
+                status.setText(R.string.invalid);
+            }
         }
     }
 }
