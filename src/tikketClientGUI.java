beff5c72457d05622d59610d45b925518d124660
max-stockhausen <info@max-stockhausen.de>
2019-03-25 08:34:28 +0100

    GUI weitergamcht

--- src/tikketClientGUI.java
+++ src/tikketClientGUI.java
@@ -9,6 +9,13 @@ import javax.swing.*;
 public class tikketClientGUI {
     private JPanel GUIeinlass;
     private JTabbedPane tabbedPane1;
+    private JPanel panelEinlass;
+    private JPanel panelAuslass;
+    private JPanel panelTickets;
+    private JPanel panelVeranstaltungen;
+    private JPanel panelInfo;
+    private JPanel panelStatus;
+    private JLabel status;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
