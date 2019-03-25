14a3fcadffafe310e2bd5cc8bddff0ae958aa2e1
max-stockhausen <info@max-stockhausen.de>
2019-03-25 08:30:01 +0100

    neue GUI. initial commit

---
+++ src/tikketClientGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Informatikprojekt aus 2019. Erstellt von Simon, und Max.
+ * Zuletzt bearbeitet 25.03.19 08:25.
+ * Keiner klaut das hier! (c) 2019.
+ */
+
+import javax.swing.*;
+
+public class tikketClientGUI {
+    private JPanel GUIeinlass;
+    private JTabbedPane tabbedPane1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
