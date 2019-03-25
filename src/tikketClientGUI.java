d683e615cf9eb403a09272ab92e617b2ed1969f2
max-stockhausen <info@max-stockhausen.de>
2019-03-25 11:10:19 +0100

    GUI weitergamcht

--- src/tikketClientGUI.java
+++ src/tikketClientGUI.java
@@ -7,17 +7,35 @@
 import javax.swing.*;
 
 public class tikketClientGUI {
-    private JPanel GUIeinlass;
-    private JTabbedPane tabbedPane1;
+    private JPanel tikketClientGUI;
+    private JTabbedPane tabPane;
     private JPanel panelEinlass;
     private JPanel panelAuslass;
     private JPanel panelTickets;
     private JPanel panelVeranstaltungen;
     private JPanel panelInfo;
     private JPanel panelStatus;
-    private JLabel status;
+    private JLabel lStatistik;
+    private JLabel lScanergebnis;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    public static void main(String[] args) {
+        try {
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+        } catch (InstantiationException e) {
+        e.printStackTrace();
+        } catch (IllegalAccessException e) {
+        e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+        e.printStackTrace();
+        }
+
+        JFrame frame = new JFrame("tikketClientGUI");
+        frame.setContentPane(new tikketClientGUI().tikketClientGUI);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
